237385699fc5655856cd76d402afed35a365be78
HasnainAbass <hasnainabass243@gmail.com>
2024-04-05 10:33:12 +0500

    initial file!

---
+++ a7has-time-zone/index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Please Enter your Name !",
+        name: "userName",
+        type: "string"
+    },
+    {
+        message: "Plaese Enter Time according to your time zone(24hrs-Format):",
+        name: "timeZone",
+        type: "number"
+    }
+]);
+// logical statments:
+if (answer.timeZone >= 5.00 && answer.timeZone <= 11.59) {
+    console.log(`Good Mornning, Mr/Ms .${answer.userName}`);
+}
+else if (answer.timeZone >= 12.00 && answer.timeZone <= 16.59) {
+    console.log(`Good Afternoon, Mr/Ms .${answer.userName}`);
+}
+else if (answer.timeZone >= 17.00 && answer.timeZone <= 20.59) {
+    console.log(`Good Evening, Mr/Ms .${answer.userName}`);
+}
+else {
+    console.log(`Good Night, Mr/Ms .${answer.userName}`);
+}
